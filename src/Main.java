2adfd602fd55e2ba2c2c99d0bfda07096383c3b9
Patricio Frettes Bazante <130622561+patriciofrettes@users.noreply.github.com>
2023-12-01 06:23:15 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 35;
+        int num2 = 20;
+        int aux;
+        System.out.println("-----Antes-----");
+        System.out.println("Num1: " + num1);
+        System.out.println("Num2: " + num2);
+        aux = num2;
+        num2 = num1;
+        num1 = aux;
+        System.out.println("-----Después-----");
+        System.out.println("Num1: " + num1);
+        System.out.println("Num2: " + num2);
+
+    }
+}
